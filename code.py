d9a690e085a6a3f0f23cea00f8069ad8d4f9faeb
noah-mccaskill <94465036+noah-mccaskill@users.noreply.github.com>
2022-06-06 11:03:08 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+#!user/usr/bin/env python3
+
+# Created by: Noah McCaskill
+# Created on: June 2022
+# This program is the "Space Aliens" program on the Pybadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this function is the main game game_scene
+    
+    print("\n\n\n") # 3 blank lines
+    print ("Hello, World!")
+    
+    #repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
